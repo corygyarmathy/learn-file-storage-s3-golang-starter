5f8847329c60f2d1c588f037bcf31a0ea52c5a16
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-12-09 18:37:38 +0800

    feat: process videos before being uploaded, moving moov atom to the start of the file

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -71,32 +73,32 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 
 	key := getAssetKey(mediaType)
 
-	dst, err := os.CreateTemp("", "tubely-upload.mp4")
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to create file on server", err)
 		return
 	}
 	defer func() {
-		if cerr := os.Remove(dst.Name()); cerr != nil {
+		if cerr := os.Remove(tempFile.Name()); cerr != nil {
 			log.Printf("Failed to remove temp video file: %v", cerr)
 		}
 	}()
 	defer func() {
-		if cerr := dst.Close(); cerr != nil {
+		if cerr := tempFile.Close(); cerr != nil {
 			log.Printf("Failed to close temp video file: %v", cerr)
 		}
 	}()
-	if _, err = io.Copy(dst, file); err != nil {
+	if _, err = io.Copy(tempFile, file); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error saving file", err)
 		return
 	}
 
-	if _, err = dst.Seek(0, io.SeekStart); err != nil {
+	if _, err = tempFile.Seek(0, io.SeekStart); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to seek start of temp video file", err)
 		return
 	}
 
-	orientation, err := getVideoOrientation(dst.Name())
+	orientation, err := getVideoOrientation(tempFile.Name())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to get video orientation", err)
 		return
@@ -104,10 +106,31 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 
 	key = path.Join(orientation, key)
 
+	processedVideoPath, err := processVideoForFastStart(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to process video for fast start", err)
+		return
+	}
+	processedVideo, err := os.Open(processedVideoPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to open processed video", err)
+		return
+	}
+	defer func() {
+		if cerr := os.Remove(processedVideo.Name()); cerr != nil {
+			log.Printf("Failed to remove temp processed video file: %v", cerr)
+		}
+	}()
+	defer func() {
+		if cerr := processedVideo.Close(); cerr != nil {
+			log.Printf("Failed to close temp processed video file: %v", cerr)
+		}
+	}()
+
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
 		Key:         &key,
-		Body:        dst,
+		Body:        processedVideo,
 		ContentType: &mediaType,
 	})
 	if err != nil {
@@ -125,4 +148,22 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 
 	respondWithJSON(w, http.StatusOK, video)
 
+}
+
+func processVideoForFastStart(filePath string) (string, error) {
+	outputPath := filePath + ".processing"
+
+	cmd := exec.Command("ffmpeg",
+		"-i", filePath,
+		"-c", "copy",
+		"-movflags", "faststart",
+		"-f", "mp4",
+		outputPath)
+	var buffer bytes.Buffer
+	cmd.Stdout = &buffer
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("running ffmpeg command: %v", err)
+	}
+
+	return outputPath, nil
 }
